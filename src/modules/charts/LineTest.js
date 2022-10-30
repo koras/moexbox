4f21938c0a56238f499d5ee0e2b670d527ce0f79
koras <kos6476@yandex.ru>
2022-10-30 20:04:29 +0300

    add redux

--- src/modules/charts/LineTest.js
+++ src/modules/charts/LineTest.js
@@ -10,7 +10,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
+//import faker from 'faker';
 
 ChartJS.register(
   CategoryScale,
@@ -23,32 +23,94 @@ ChartJS.register(
 );
 
 export const options = {
+ 
+   
   responsive: true,
   plugins: {
+    id: 'myEventCatcher', 
     legend: {
-      position: 'top',
+   //   position: 'top',
     },
     title: {
-      display: true,
+      display: false,
       text: 'Chart.js Line Chart',
     },
+
+    subtitle: {
+      display: false,
+      text: 'Custom Chart Subtitle'
+  }
+
+
+
+  },
+  scales: {
+    y: {
+        beginAtZero: true
+    }
+  },
+  elements:{
+    point:{
+      radius: 4,
+      hitRadius:4
+    },
+    line:{
+      backgroundColor:'rgba(255, 159, 64, 1)'
+    }
+  },
+  interaction:{
+    mode: 'index',
+  },
+  
+  events: ['mousemove', 'mouseout', 'click'],
+
+  onClick : (event, items) =>{
+    if(items && items[0]){ 
+    console.log(items);
+    console.log(items[0].index);
+  }
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'February', 'March', 'April', 'May', 'June', 'July', 'February', 'March', 'April', 'May', 'June', 'July', 'February', 'March', 'April', 'May', 'June', 'July', 'February', 'March', 'April', 'May', 'June', 'July', 'February', 'March', 'April', 'May', 'June', 'July'];
-
 export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
+  fill: true,
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets:  [{
+    fill: true,
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    
+    backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ],
+    borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ],
+    borderWidth: 1,
+    tension: 0.1
+}] 
 };
 
+
+
 export function LineTest() {
-  return <Line options={options} data={data} />;
+  //const chartRef = useRef();
+
+ 
+
+  return <Line options={options} data={data}  
+  
+
+  
+  />;
 }
