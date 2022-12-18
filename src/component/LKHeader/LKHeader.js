199e3f15759a32a0759f267127aa03967f02b329
koras <kos6476@yandex.ru>
2022-12-18 23:11:55 +0300

    auth

--- src/component/LKHeader/LKHeader.js
+++ src/component/LKHeader/LKHeader.js
@@ -11,12 +11,8 @@ import Button from "@mui/material/Button";
 import './style.css';
   
 
-// {"web":{"client_id":"854331110586-ams6lqojo4dc3buer6ia5ov7jtjo92ca.apps.googleusercontent.com",
-// "project_id":"moexbox","auth_uri":"https://accounts.google.com/o/oauth2/auth",
-// "token_uri":"https://oauth2.googleapis.com/token",
-// "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-// "client_secret":"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"}}
 
+const iconGoogle = <svg aria-hidden="true" class="native svg-icon iconGoogle" width="18" height="18" viewBox="0 0 18 18"><path d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18Z" fill="#4285F4"></path><path d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 0 1-7.18-2.54H1.83v2.07A8 8 0 0 0 8.98 17Z" fill="#34A853"></path><path d="M4.5 10.52a4.8 4.8 0 0 1 0-3.04V5.41H1.83a8 8 0 0 0 0 7.18l2.67-2.07Z" fill="#FBBC05"></path><path d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 0 0 1.83 5.4L4.5 7.49a4.77 4.77 0 0 1 4.48-3.3Z" fill="#EA4335"></path></svg>
 
 const clientId = '854331110586-ams6lqojo4dc3buer6ia5ov7jtjo92ca.apps.googleusercontent.com';
 var SCOPES = "https://accounts.google.com/o/oauth2/auth";
@@ -58,50 +54,54 @@ export const LKHeader =() => {
 const  GetProfile =()=> {
   if(profile && profile.name){
     console.log('user image',profile);
-    return  <div>
-            <img src={profile.imageUrl} alt="user image" />
+    return  <div className='profile-box' >
+            
+            <div className="moex-button-profile"> В профиль </div>   
+             
+
             {/* <h3>User Logged in</h3>
             <p>Name: {profile.name}</p>
             <p>Email Address: {profile.email}</p> */}
       
             <GoogleLogout clientId={clientId} 
-                render={renderProps => (
-                  <Button
+                 render={renderProps => (
+                  <div className='profile-box'> 
+                    <div    
                     type="submit"
-                    fullWidth
-                    variant="contained"
+
+          
                     color="primary"
-                    className={customStyle}
+                    className="auth-google"
                     onClick={renderProps.onClick}
                   >
-                    
-                </Button>
-                
+                    {iconGoogle}
+                   <span>Выйти</span>
+                   </div> 
+                   </div>
                 )}
             className={customStyle}
-            buttonText="Выйти" 
+
            
             onLogoutSuccess={logOut} />
+  
         </div>
     } else {
       return <div><GoogleLogin
               render={renderProps => (
-                <Button
-                size="small"  
-                 variant="outlined"
-                  type="submit"
-                  fullWidth
+                <div > <div    
+                  type="submit" 
            
                   color="primary"
-                  className={customStyle}
+                  className="auth-google"
                   onClick={renderProps.onClick}
                 >
-                  Авторизироваться
-              </Button>
+                  {iconGoogle}
+                 <span>Войти в профиль</span>
+              </div>
+              </div>
               )}
             clientId={clientId}
-            buttonText="Войти "
-            className={customStyle}
+            buttonText="Войти " 
             onSuccess={onSuccess}
             onFailure={onFailure}
             cookiePolicy={'single_host_origin'}
